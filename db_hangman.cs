f1767b9e4bb2708836a4cf4ab459c3817f6a6a64
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-03-29 09:32:25 -0700

    Update db_hangman.cs; identify issues

--- db_hangman.cs
+++ db_hangman.cs
@@ -22,7 +22,48 @@
 //   * I indiscriminately add or change comments according to my whims.
 //   * I eliminate unnecessary using statements.
 
-// FUNCTIONAL MODIFICATIONS:
+// FUNCTIONAL ISSUES (soon to be MODIFICATIONS):
+//   * There is an infinite loop in the program (i.e. a while(true)
+//     statement that does not contain a break or a return statement.
+//     As a consequence of this, there is no graceful way for the user
+//     to terminate the program. The program does not detect when the
+//     user has solved the puzzle.
+//
+//   * The program does not properly handle user input.  If the user
+//     enters more than one character at a time, the program crashes.
+//     "System.FormatException: String must be exactly one character long"
+//     is not a friendly message. The user gets the same message when
+//     he/she does not enter any input.
+//
+//   * The program does not properly prompt the user for input.
+//     The word mask should be displayed before every request for input,
+//     and the user should prompted for every input.
+//
+//   * ALL of the program functionality is stuffed in the Main() function.
+//     Also, that program functionality is not broken up into bite-sized
+//     logical functions. 
+//
+//   * There is no loss condition in the program.  The program does not 
+//     detect when the user has failed to solve the puzzle.
+//
+//   * Besides the fact that the program does not detect when the user
+//     has solved or not solved the puzzle, it does not provide a way to
+//     replay the game... other than re-running the program from the 
+//     command line.
+//
+//   * Could the word list be stored in a data file, rather than
+//     hard-coded into the program?
+//
+//   * In the word mask, let's use dashes rather than asterisks.
+//
+//   * Playing Hangman, the user really expects to see a gallows and a
+//     graphical representation of their progress towards winning or
+//     losing the game.
+//
+//   * For my CSC200 students, let's split up the Hangman class into 
+//     a non-object-oriented Startup class and an object-oriented
+//     HangmanGame class. Create objects with methods, and
+//     where desirable, eliminate static functions.
 
 
 using System;
@@ -33,10 +74,9 @@ namespace mypgms {
 class Hangman {
 
     //----------
-    public static void Main(string[] args) {
+    static void Main(string[] args) {
  
         Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
-
         string[] listwords = new string[10];
         listwords[0] = "sheep";
         listwords[1] = "goat";
@@ -48,14 +88,10 @@ class Hangman {
         listwords[7] = "pineapple";
         listwords[8] = "orange";
         listwords[9] = "mango";
-
         Random randGen = new Random();
         var idx = randGen.Next(0, 9);
-
         string mysteryWord = listwords[idx];
-
         char[] guess = new char[mysteryWord.Length];
-
         Console.Write("Please enter your guess: ");
  
         for (int p = 0; p < mysteryWord.Length; p++) {
@@ -74,4 +110,4 @@ class Hangman {
     } // end function Main()
 } // end class Hangman
 
-} // end namespace mypgms:
+} // end namespace mypgms
