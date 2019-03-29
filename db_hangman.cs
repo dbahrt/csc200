f087a0ec908eab135439213469947976eeb59b32
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-03-29 09:26:57 -0700

    Update hangman.cs; make style modifications

--- db_hangman.cs
+++ db_hangman.cs
@@ -1,4 +1,3 @@
-//345678901234567890123456789012345678901234567890123456789012345678901234567890
 // SOURCE: https://www.sanfoundry.com/csharp-programs-gaming-hangman/
 // AUTHOR: Manish Bhojasia, a technology veteran with 20+ years @ Cisco &
 //     Wipro, [along with consultancies at IBM, Brocade, Quantum, etc.]
@@ -10,57 +9,69 @@
 // STUDENT: Dan Bahrt
 // DATE: 08 Feb 2019
 
-// STYLE MODIFICATIONS: none
+// STYLE MODIFICATIONS:
+//   * I use 1tbs indentation style (4 spaces per level),
+//     with mandatory braces for all control structures.
+//     I do not indent classes definitions within namespaces.
+//   * I add minimal "flower box" comments to set apart
+//     class and function definitions.
+//   * I put "// end" comments on closing braces for
+//     namespaces, classes, and functions.
+//   * To better document their purposes, I freely change the names of
+//     namespaces, classes, functions, variables and constants.
+//   * I indiscriminately add or change comments according to my whims.
+//   * I eliminate unnecessary using statements.
 
-// FUNCTIONAL MODIFICATIONS: none
+// FUNCTIONAL MODIFICATIONS:
 
 
-/*
- *  C# Program to Create a HangMan Game
- */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
  
-namespace Hangman
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace mypgms {
+
+//==========
+class Hangman {
+
+    //----------
+    public static void Main(string[] args) {
  
-            Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
-            string[] listwords = new string[10];
-            listwords[0] = "sheep";
-            listwords[1] = "goat";
-            listwords[2] = "computer";
-            listwords[3] = "america";
-            listwords[4] = "watermelon";
-            listwords[5] = "icecream";
-            listwords[6] = "jasmine";
-            listwords[7] = "pineapple";
-            listwords[8] = "orange";
-            listwords[9] = "mango";
-            Random randGen = new Random();
-            var idx = randGen.Next(0, 9);
-            string mysteryWord = listwords[idx];
-            char[] guess = new char[mysteryWord.Length];
-            Console.Write("Please enter your guess: ");
+        Console.WriteLine("Welcome to Hangman!!!!!!!!!!");
+
+        string[] listwords = new string[10];
+        listwords[0] = "sheep";
+        listwords[1] = "goat";
+        listwords[2] = "computer";
+        listwords[3] = "america";
+        listwords[4] = "watermelon";
+        listwords[5] = "icecream";
+        listwords[6] = "jasmine";
+        listwords[7] = "pineapple";
+        listwords[8] = "orange";
+        listwords[9] = "mango";
+
+        Random randGen = new Random();
+        var idx = randGen.Next(0, 9);
+
+        string mysteryWord = listwords[idx];
+
+        char[] guess = new char[mysteryWord.Length];
+
+        Console.Write("Please enter your guess: ");
  
-            for (int p = 0; p < mysteryWord.Length; p++)
-                guess[p] = '*';
- 
-            while (true)
-            {
-                char playerGuess = char.Parse(Console.ReadLine());
-                for (int j = 0; j < mysteryWord.Length; j++)
-                {
-                    if (playerGuess == mysteryWord[j])
-                        guess[j] = playerGuess;
-                }
-                Console.WriteLine(guess);
-            }
+        for (int p = 0; p < mysteryWord.Length; p++) {
+            guess[p] = '*';
         }
-    }
-}
+ 
+        while (true) {
+            char playerGuess = char.Parse(Console.ReadLine());
+            for (int j = 0; j < mysteryWord.Length; j++) {
+                if (playerGuess == mysteryWord[j]) {
+                    guess[j] = playerGuess;
+                }
+            }
+            Console.WriteLine(guess);
+        }
+    } // end function Main()
+} // end class Hangman
+
+} // end namespace mypgms:
