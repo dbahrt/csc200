a140b9e78872a9d469f04872b92690049bc46876
Dan Bahrt <28609248+dbahrt@users.noreply.github.com>
2019-11-08 06:47:34 -0800

    major documentation changes & code restructuring

--- polymorph02.cs
+++ polymorph02.cs
@@ -1,38 +1,23 @@
-// source: taken from https://www.youtube.com/watch?v=4a_iTOtGhM8 
+// SOURCE: taken from https://www.youtube.com/watch?v=4a_iTOtGhM8 
 //     Part 23 - C# Tutorial - Polymorhpism in c#.avi
-// author: kudvenkat
-// published on Jun 9, 2012
 // Text version of the video at http://csharp-video-tutorials.blogspot.com/...
 //     /2012/06/part-23-c-tutorial-polymorphism.html
-// student: Dan Bahrt
-// synopsis: simple inheritance; creating array that demonstrates
+// AUTHOR: kudvenkat
+// PUBDATE: Jun 9, 2012
+// STUDENT: Dan Bahrt
+// DOWNLOAD: Mar 29, 2019
+// SYNOPSIS: simple inheritance; creating array that demonstrates
 //     simple polymorphism (treating derived class object as base class object)
 
 using System;
 
-public class Employee {
-    public string FirstName = "FN";
-    public string LastName = "LN";
-
-    public void PrintFullName() {
-        Console.WriteLine(FirstName + " " + LastName + " base default");
-    }
-}
-
-public class FullTimeEmployee : Employee {
-}
-
-public class PartTimeEmployee : Employee {
-}
-
-public class TemporaryEmployee : Employee {
-}
-
-public class InternEmployee : Employee {
-}
-
+//==========
 public class Program {
+
+    //----------
     public static void Main() {
+        // instantiate objects and shove reference pointers into 
+        // array of base class objects.
         Employee [] employees = new Employee[5];
         employees[0] = new FullTimeEmployee();
         employees[1] = new PartTimeEmployee();
@@ -40,12 +25,65 @@ public class Program {
         employees[3] = new InternEmployee();
         employees[4] = new Employee();
 
+        // loop through array of base class objects and 
+        // call member methods that give evidence of execution.
         foreach(Employee e in employees) {
             e.PrintFullName();
         }
     }
 }
 
-/* SAMPLE OUTPUT:
+//==========
+// base class definition
+//==========
+public class Employee {
+    public string FirstName = "FN";
+    public string LastName = "LN";
 
+    //----------
+    public void PrintFullName() {
+        Console.WriteLine(FirstName + " " + LastName + " base default");
+    }
+}
+
+//==========
+// FullTimeEmployee derived class definition
+//==========
+public class FullTimeEmployee : Employee {
+}
+
+//==========
+// PartTimeEmployee derived class definition
+//==========
+public class PartTimeEmployee : Employee {
+}
+
+//==========
+// TemporaryEmployee derived class definition
+//==========
+public class TemporaryEmployee : Employee {
+}
+
+//==========
+// InternEmployee derived class definition
+//==========
+public class InternEmployee : Employee {
+}
+
+// MODIFICATIONS:  MINIMAL.  added comments.  minor code restructuring.
+
+// ANALYSIS: instantiate base class object and several derived class objects
+// that at this point are indistinguishable from each other.
+// all of the derived objects inherit all of the members of the base class,
+// but none of them differentiate themselves in any way from the base class.
+// shove all of these objects into an array of base class objects.
+// loop through array of base class objects and 
+// call member methods that give evidence of execution.
+
+/* SAMPLE OUTPUT:
+FN LN base default
+FN LN base default
+FN LN base default
+FN LN base default
+FN LN base default
 */
